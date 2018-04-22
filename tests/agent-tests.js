2c2ca61d8e85401544e27215513e018fcdc87743
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-04-21 23:55:18 -0300

    Code coverage with nyc

--- tests/agent-tests.js
+++ tests/agent-tests.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
-const test = require('ava');
+const test = require('ava')
 
 let config = {
   logging: function() {}
-};
-let db = null;
+}
+let db = null
 
 test.beforeEach(async () => {
-  const setupDatabase = require('../');
-  db = await setupDatabase(config);
-});
+  const setupDatabase = require('../')
+  db = await setupDatabase(config)
+})
 
 test('Agent', (t) => {
-  t.truthy(db.agent, 'Agent service should exist');
-});
+  t.truthy(db.agent, 'Agent service should exist')
+})
