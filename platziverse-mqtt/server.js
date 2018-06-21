6c5f59f308dd0a269440905563fc4a9d8fe60345
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-06-21 01:58:48 -0300

    [Mqtt] Save agent and metric information in db

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -5,6 +5,7 @@ const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
 const db = require('platziversedb')
+const { parsePayload } = require('./utils')
 
 const backend = {
   type: 'redis',
@@ -36,12 +37,93 @@ server.on('clientConnected', client => {
   clients.set(client.id, null)
 })
 
-server.on('clientDisconnected', client => {
+server.on('clientDisconnected', async client => {
   debug(`Client Disconnected: ${client.id}`)
+  const agent = clients.get(client.id)
+  
+  if (agent) {
+    // Mark as disconnected
+    agent.connected = false
+
+    try {
+      await Agent.createOrUpdate(agent)
+    } catch (e) {
+      return handleError(e)
+    }
+
+    // Delete agent from client list
+    clients.delete(client.id)
+    server.publish({
+      topic: 'agent/disconnected',
+      payload: JSON.stringify({
+        agent: {
+          uuid: agent.uuid
+        }
+      })
+    })
+
+    debug(`Client ${client.id} associated to agent ${agent.uuid} was mark as disconnected`)
+  }
 })
 
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
   debug(`Received: ${packet.topic}`)
+
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message': {
+      debug(`Payload: ${packet.payload}`)
+      const payload = parsePayload(packet.payload)
+      if (payload) {
+        payload.agent.connected = true
+
+        let agent
+
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+
+        debug(`Agent ${agent.uuid} was saved`)
+
+        // Notify new connected agent
+        if (!clients.get(client.id)) {
+          clients.set(client.id)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                username: agent.username,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+
+        // Store metrics
+        for (let metric of payload.metrics) {
+          let m
+
+          try {
+            m = await Metric.create(agent.uuid, metric)
+          } catch (e) {
+            return handleError(e)
+          }
+
+          debug(`Metric ${m.id} was saved on Agent ${agent.uuid}`)
+        }
+      }
+      break
+    }
+  }
 })
 
 server.on('ready', async () => {
@@ -53,11 +135,16 @@ server.on('ready', async () => {
 
 server.on('error', handleFatalError)
 
+function handleError (err) {
+  console.error(`${chalk.red('[error]')} ${err.message}`)
+  console.error(err.stack)
+}
+
 function handleFatalError (err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
 
-process.on('uncaughtException', handleFatalError) 
+process.on('uncaughtException', handleFatalError)
 process.on('unhandledRejection', handleFatalError)
