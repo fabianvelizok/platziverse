5fa2be79791c453c5f7350e85c72d59670255ba8
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-06-20 23:55:43 -0300

    [Agent] Add README file and a linter

--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,1 +1,11 @@
-'use strict';
+'use strict'
+
+const EventEmitter = require('events')
+
+class PlatziverseAgent extends EventEmitter {
+  constructor() {
+    super()
+  }
+}
+
+module.exports = PlatziverseAgent
