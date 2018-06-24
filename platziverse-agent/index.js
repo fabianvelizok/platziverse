17f3c13f0661a33033d89b4db6f037e3f9990e71
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-06-23 22:47:14 -0300

    [Agent] Define a timer to notify events

--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -3,8 +3,30 @@
 const EventEmitter = require('events')
 
 class PlatziverseAgent extends EventEmitter {
-  constructor() {
+  constructor(opts) {
     super()
+
+    this._options = opts
+    this._timer = null
+    this._started = false
+  }
+
+  connect() {
+    if (!this._started) {
+      this.emit('connected')
+      this._started = true
+      this._timer = setInterval(() => {
+        this.emit('agent/message', 'A message')
+      }, this._options.interval)
+    }
+  }
+
+  disconnect() {
+    if (this._started) {
+      this.emit('disconnected')
+      clearInterval(this._timer)
+      this._started = false
+    }
   }
 }
 
