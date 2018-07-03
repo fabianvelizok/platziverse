7f0a596896bcdaa2e6de440c3a55788ec9074002
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-07-02 22:48:05 -0300

    [API] Add some integration tests

--- platziverse-api/tests/api-tests.js
+++ platziverse-api/tests/api-tests.js
@@ -2,18 +2,71 @@
 
 const test = require('ava')   
 const request = require('supertest') // Supertest works with callbacks
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+const common = require('platziverse-common')
+const config = require('../config')
+const util = require('util')
 
-const server = require('..')
+const agentFixtures = common.tests.fixtures.agent
+const auth = require('../auth')
+const sign = util.promisify(auth.sign)
+
+let sandbox = null
+let server = null
+let dbStub = null
+let token = null
+let AgentStub = {}
+let MetricStub = {}
+
+test.beforeEach(async () => {
+  sandbox = sinon.createSandbox()
+  
+  dbStub = sandbox.stub()
+  dbStub.returns(Promise.resolve({
+    Agent: AgentStub,
+    Metric: MetricStub
+  }))
+
+  AgentStub.findConnected = sinon.stub()
+  AgentStub.findConnected.returns(Promise.resolve(agentFixtures.connected))
+
+  token = await sign({ admin: true, username: 'platzi'}, config.auth.secret)
+  const api = proxyquire('../api', {
+    'platziverse-db': dbStub
+  })
+
+  server = proxyquire('../server', {
+    './api': api
+  })
+})
+
+test.afterEach(async () => {
+  sandbox && sandbox.restore()
+})
 
 test.serial.cb('/api/agents', t => {
   request(server)
     .get('/api/agents')
+    .set('Authorization', `Bearer ${token}`)
     .expect(200)
     .expect('Content-Type', /json/)
     .end((err, res) => {
       t.falsy(err, 'It should not return an error')
-      let body = res.body
-      t.deepEqual(body, {}, 'Body should be expected')
+      let body = JSON.stringify(res.body)
+      let expected = JSON.stringify(agentFixtures.connected)
+      t.deepEqual(body, expected, 'Body should be expected')
       t.end()
     })
 })
+
+test.serial.todo('/api/agents - not authorized')
+
+test.serial.todo('/api/agent/:uuid')
+test.serial.todo('/api/agent/:uuid - not found')
+
+test.serial.todo('/api/metrics/:uuid')
+test.serial.todo('/api/metrics/:uuid - not found')
+
+test.serial.todo('/api/metrics/:uuid/:type')
+test.serial.todo('/api/metrics/:uuid/:type - not found')
