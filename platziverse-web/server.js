67710399de8c70a975156de31356cdfc3a2dfaf8
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-07-08 23:15:29 -0300

    [Web] Integrate agent module with socket.io

--- platziverse-web/server.js
+++ platziverse-web/server.js
@@ -6,31 +6,30 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socket = require('socket.io')
+const PlatziverseAgent = require('platziverse-agent')
+
+const { pipe } = require('./utils')
 
 const port = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
 const io = socket(server)
+const agent = new PlatziverseAgent()
 
 io.on('connect', socket => {
   debug(`Connected ${socket.id}`)
 
-  socket.on('agent/message', payload => {
-    console.log(payload)
-  })
-
-  setInterval(() => {
-    socket.emit('agent/message', { agent: 'xyz'})
-  }, 5000)
+  pipe(agent, socket)
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 server.listen(port, () => {
   console.log(`${chalk.green('[platziverse-web]')} server listening on port ${port}`)
+  agent.connect()
 })
 
-function handleFatalError(err) {
+function handleFatalError (err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
