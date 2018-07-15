cbbaac880c8254545f87bb4999001da82bb54d84
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-07-15 03:45:43 -0300

    [CLI] Show agents and metrics in console

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -6,6 +6,17 @@
 
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
+const moment = require('moment')
+const PlatziverseAgent = require('platziverse-agent')
+
+const agent = new PlatziverseAgent()
+const agents = new Map()
+const agentMetrics = new Map()
+let extended = []
+let selected = {
+  uuid: null,
+  type: null
+}
 
 const screen = blessed.screen()
 const grid = new contrib.grid({ rows: 1, cols: 4, screen: screen })
@@ -20,8 +31,133 @@ const line = grid.set(0, 1, 1, 3, contrib.line, {
   xPadding: 5
 })
 
+agent.on('agent/connected', payload => {
+  const { uuid } = payload.agent
+
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent)
+    agentMetrics.set(uuid, {})
+  }
+
+  renderData()
+})
+
+agent.on('agent/disconnected', payload => {
+  const { uuid } = payload.agent
+
+  if (agents.has(uuid)) {
+    agents.delete(uuid)
+    agentMetrics.delete(uuid)
+  }
+
+  renderData()
+})
+
+agent.on('agent/message', payload => {
+  const { uuid } = payload.agent
+  const { timestamp } = payload
+
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent)
+    agentMetrics.set(uuid, {})
+  }
+
+  const metrics = agentMetrics.get(uuid)
+
+  payload.metrics.forEach(m => {
+    const { type, value } = m
+
+    if (!Array.isArray(metrics[type])) {
+      metrics[type] = []
+    }
+
+    const length = metrics[type].length
+    if (length >= 20) {
+      metrics[type].shift()
+    }
+
+    metrics[type].push({
+      value,
+      timestamp: moment(timestamp).format('HH:mm:ss')
+    })
+  })
+
+  renderData()
+})
+
+tree.on('select', node => {
+  const { uuid, type } = node
+
+  if (node.agent) {
+    node.extended ? extended.push(uuid) : extended = extended.filter(e => e.uuid !== uuid)
+    selected.uuid = null
+    selected.type = null
+    return
+  }
+
+  selected.uuid = uuid
+  selected.type = type
+
+  renderMetric()
+})
+
+function renderData () {
+  const treeData = {}
+  let idx = 0
+  for (let [uuid, val] of agents) {
+    const title = `${val.name} - (${val.pid})`
+    treeData[title] = {
+      uuid,
+      agent: true,
+      extended: extended.includes(uuid),
+      children: {}
+    }
+
+    const metrics = agentMetrics.get(uuid)
+
+    Object.keys(metrics).forEach(type => {
+      const metric = {
+        uuid,
+        type,
+        metric: true
+      }
+
+      const metricName = ` ${type} ${' '.repeat(1000)} ${idx += 1}`
+      treeData[title].children[metricName] = metric
+    })
+  }
+
+  tree.setData({
+    extended: true,
+    children: treeData
+  })
+
+  renderMetric()
+}
+
+function renderMetric () {
+  if (!selected.uuid && !selected.type) {
+    line.setData([{x: [], y: [], title: ''}])
+    screen.render()
+    return
+  }
+
+  const metrics = agentMetrics.get(selected.uuid)
+  const values = metrics[selected.type]
+  const series = [{
+    title: selected.type,
+    x: values.map(v => v.timestamp).slice(-10),
+    y: values.map(v => v.value).slice(-10)
+  }]
+
+  line.setData(series)
+  screen.render()
+}
+
 screen.key(['escape', 'q', 'C-c'], (ch, key) => {
   process.exit(0)
 })
 
+agent.connect()
+tree.focus()
 screen.render()
